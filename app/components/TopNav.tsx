143d5b85532ae97bbc26e08ed209f2594b096aa6
Wilco Mulder <Wilcomulder@hotmail.com>
2026-01-17 14:51:38 +0100

    new user control

--- app/components/TopNav.tsx
+++ app/components/TopNav.tsx
@@ -263,7 +263,7 @@ export default function TopNav() {
                   className="w-full text-left px-3 py-2 text-sm hover:bg-gray-50"
                   onClick={() => {
                     setUserMenuOpen(false);
-                    router.push("/invites");
+                    router.push("/admin/users");
                   }}
                 >
                   Users
